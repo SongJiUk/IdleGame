10feab36759386c1efd0adec46c7d3ba2ce0720e
JiUk <wldnr1014@naver.com>
2025-11-28 15:29:57 +0900

    나머지 업로드

--- IdleGame/Assets/@Scripts/Utils/EditorStartInit.cs
+++ IdleGame/Assets/@Scripts/Utils/EditorStartInit.cs
@@ -8,10 +8,20 @@ using UnityEngine;
 [InitializeOnLoad]
 public class EditorStartInit
 {
-    static EditorStartInit()
+
+    [MenuItem("MyEditor/시작 씬부터 시작")]
+    public static void SetupFromStartScene()
     {
         var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
         var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
         EditorSceneManager.playModeStartScene = sceneAsset;
+        UnityEditor.EditorApplication.isPlaying = true;
+    }
+
+    [MenuItem("MyEditor/현재 씬부터 시작")]
+    public static void StartFromThisScene()
+    {
+        EditorSceneManager.playModeStartScene = null;
+        UnityEditor.EditorApplication.isPlaying = true;
     }
 }
