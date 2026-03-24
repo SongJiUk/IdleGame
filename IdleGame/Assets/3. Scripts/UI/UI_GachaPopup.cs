04ba58e3806663dcda89fa6208e65be313f7c01b
Jiuk <wldnr1014@naver.com>
2026-03-24 19:56:07 +0900

    수정 2

--- IdleGame/Assets/3. Scripts/UI/UI_GachaPopup.cs
+++ IdleGame/Assets/3. Scripts/UI/UI_GachaPopup.cs
@@ -83,17 +83,6 @@ public class UI_GachaPopup : UI_Popup
             int horizontalCount = 0;
             value = _count;
 
-            if (!isAd)
-            {
-                int cost = _count == 11 ? 3000 : 300;
-                if (Managers.GameM.Dia < cost)
-                {
-                    Managers.UIM.ShowToast(Managers.LocalizationM.Get("UIToastNoDia"));
-                    return;
-                }
-                Managers.GameM.Dia -= cost;
-            }
-
             if (isAd)
             {
                 GetText(TextsType, (int)Texts.OneMoreButtonText).text = Managers.LocalizationM.Get("UIShop_1Summon");
@@ -230,6 +219,13 @@ public class UI_GachaPopup : UI_Popup
         }
         else
         {
+            int cost = value == 11 ? 3000 : 300;
+            if (Managers.GameM.Dia < cost)
+            {
+                Managers.UIM.ShowToast(Managers.LocalizationM.Get("UIToastNoDia"));
+                return;
+            }
+            Managers.GameM.Dia -= cost;
             ResetIcon();
             Managers.RenderM.renderGacha.ClearList();
             GetGachaHero(value).Forget();
